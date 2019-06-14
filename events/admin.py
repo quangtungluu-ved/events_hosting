89cc82c9ab481a71a40d5811201175787b094a56
quantungluu-ved <quantung.luu@ved.com.vn>
2019-06-14 17:48:29 +0700

    add channel on admins' site

--- events/admin.py
+++ events/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 
 from rest_framework.authtoken.models import Token
-from .models import Event, Comment, Channel, Image
+from .models import Event, Comment, Channel, Image, Event_Channel
 from events.tasks import notify_on_event_changes, schedule
 
 # Register models from here.
@@ -16,6 +16,12 @@ class ImageInline(admin.TabularInline):
     model = Image
     min_num = 0
     exclude = ('create_at'),
+    extra = 1
+
+
+class EventChannelInline(admin.TabularInline):
+    model = Event_Channel
+    extra = 1
 
 
 @admin.register(Event)
@@ -23,7 +29,10 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'location', 'start_date', 'end_date', )
     fields = ('title', 'description', 'location', ('start_date', 'end_date'), )
     actions_on_top = False
-    inlines = [ImageInline]
+    inlines = [
+        ImageInline,
+        EventChannelInline,
+    ]
 
     def save_model(self, request, obj, form, change):
         if not change:
